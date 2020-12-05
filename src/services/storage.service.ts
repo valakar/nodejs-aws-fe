e5373161415ceabe9298fe8ce66c0e63acc59251
Viktor Novak <valakar@gmail.com>
2020-12-05 23:55:28 +0200

    feat: fix token

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -2,7 +2,7 @@ export abstract class StorageService {
     static readonly token = 'authorization_token';
 
     static init() {
-        localStorage.setItem(StorageService.token, 'wrongToken');
+        localStorage.setItem(StorageService.token, 'dmFsYWthcjpURVNUX1BBU1NXT1JE');
     }
 
     static set(key: string, value: string): void {
