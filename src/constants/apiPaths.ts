741d43bdd7661d1bc7d865b0b6f5f721e271c5fb
Viktor Novak <valakar@gmail.com>
2020-12-05 23:46:57 +0200

    feat: integrage with cart-api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   product: 'https://lpeipbrbk3.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://yc9kze3ml1.execute-api.eu-west-1.amazonaws.com/dev/import',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://valakar-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
